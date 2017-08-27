cff78fe51080b052e2c1d5f01e6fe7053ef37c56
Iván Ruvalcaba <mario.i.ruvalcaba@gmail.com>
2017-08-27 02:22:01 -0500

    Update to 0.1.7
    - New icon
    - Replace 'chrome' namespace with 'browser'

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,35 +1,36 @@
-const contextMenuRemoveOverlayTitle = chrome.i18n.getMessage(
-        'contextMenuRemoveOverlayTitle');
+// Get the translated string for context menu.
+const contextMenuRemoveOverlayTitle = browser.i18n.getMessage(
+    'contextMenuRemoveOverlayTitle');
 
 function removeOverlay() {
     // No tabs or host permissions needed!
-    chrome.tabs.executeScript(null, {file: '/js/overlay_remover.js'},
-            function() {
-                chrome.tabs.executeScript(null,
-                        {code: 'overlayRemoverRun();'});
-            });
+    browser.tabs.executeScript(null, {file: '/js/overlay_remover.js'},
+        function() {
+            browser.tabs.executeScript(null,
+                {code: 'overlayRemoverRun();'});
+        });
 }
 
 // Create all the context menu items.
-chrome.contextMenus.create({
+browser.contextMenus.create({
     id: 'removeOverlay',
     title: contextMenuRemoveOverlayTitle
 });
 
 // The click event listener, where we perform the appropriate action given the
 // ID of the menu item that was clicked.
-chrome.contextMenus.onClicked.addListener(function(info) {
+browser.contextMenus.onClicked.addListener(function(info) {
     if (info.menuItemId === 'removeOverlay') {
         removeOverlay();
     }
 });
 
 // Shortcut listener.
-chrome.commands.onCommand.addListener(function(command) {
+browser.commands.onCommand.addListener(function(command) {
     if (command === 'removeOverlay') {
         removeOverlay();
     }
 });
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(removeOverlay);
+browser.browserAction.onClicked.addListener(removeOverlay);
